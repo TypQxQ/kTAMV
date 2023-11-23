64c3b8a3df230d2010081f8827da7ad009585ca2
TypQxQ <andrei@ignat.se>
2023-11-23 16:57:41 +0100

    q

--- install.sh
+++ install.sh
@@ -57,7 +57,7 @@ MOONRAKER_ASVC=~/printer_data/moonraker.asvc
 # The python requirements are for the installer and plugin
 # The virtualenv is for our virtual package env we create
 # The curl requirement is for some things in this bootstrap script.
-PKGLIST="python3 python3-pip virtualenv curl python3-matplotlib python3-numpy python3-opencv python3-pil libatlas-base-dev"
+PKGLIST="python3 python3-pip virtualenv python3-matplotlib python3-numpy python3-opencv python3-pil python3-flask libatlas-base-dev"
 
 
 #
@@ -498,7 +498,7 @@ verify_ready
 
 # Before we do anything, make sure our required system packages are installed.
 # These are required for other actions in this script, so it must be done first.
-# install_or_update_system_dependencies
+install_or_update_system_dependencies
 
 # Check that kTAMV isn't found.
 check_for_ktamv
